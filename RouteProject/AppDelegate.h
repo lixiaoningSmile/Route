5af9eb6e95d54daeead0499308279a808b96e4a4
smile <185896739@qq.com>
2017-06-13 16:41:26 +0800

    Release 0.0.1

---
+++ RouteProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RouteProject
+//
+//  Created by 李晓宁 on 2017/6/8.
+//  Copyright © 2017年 李晓宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RouteProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RouteProject
+//
+//  Created by 李晓宁 on 2017/6/8.
+//  Copyright © 2017年 李晓宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
